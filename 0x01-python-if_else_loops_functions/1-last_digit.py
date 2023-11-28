433b768fdf709b01e3254873bdd4bb8c09dbc80a
root <ifeanyitech15@gmail.com>
2023-11-28 05:39:22 -0800

    using abs function to check number is greater or less than

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,15 +7,15 @@ number = random.randint(-1000, 1000)
 
 #check if number is the last digit of the total number
 
-last_digit_of_a_number = abs(number) % 10
+last_d = abs(number) % 10
 
 #check if the last digit is greater than 5
 
-if last_digit_of_a_number > 5:
-    print(f'Last digit of {number} is {last_digit_of_a_number} and is greater than 5')
+if last_d > 5:
+    print(f'Last digit of {number} is {last_d} and is greater than 5')
 
-elif last_digit_of_a_number == 0:
-    print(f'Last digit of {number} is {last_digit_of_a_number} and is zero')
+elif last_d == 0:
+    print(f'Last digit of {number} is {last_d} and is zero')
 
-elif last_digit_of_a_number < 6 and not last_digit_of_a_number == 0:
-        print(f'Last digit of {number} is {last_digit_of_a_number} and is less than 6 and not 0')
+elif last_d < 6 and not last_d == 0:
+    print(f'Last digit of {number} is {last_d} and is less than 6 and not 0')
