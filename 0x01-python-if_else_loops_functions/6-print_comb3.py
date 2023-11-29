a19f1e8cd8f058833b76de74d8e4a214b04e0955
root <ifeanyitech15@gmail.com>
2023-11-29 10:35:26 -0800

    print number

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(i + 1, 10):
-        print(f'{i}{j}', end=", " if i < 8 else "")
+        print(f'{i}{j}'.format(), end=", " if i < 8 else "")
 print()
