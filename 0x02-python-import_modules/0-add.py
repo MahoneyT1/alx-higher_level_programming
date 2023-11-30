ab504d5d7d601bf33d9536700247be135c1bbd76
root <ifeanyitech15@gmail.com>
2023-11-30 13:03:06 -0800

    a program that calulates two numbers

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0
+import add_0 as add
 
 a = 1
 b = 2
 #<a value> + <b value> = <add(a, b) value>
-print(f"{a} + {b} = ", add_0.add(a, b))
+print(f"{a} + {b} = ", add.add(a, b))
