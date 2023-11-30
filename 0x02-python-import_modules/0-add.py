f22c853eeda70105a7e01fcc8a6ea7c7cab43a96
root <ifeanyitech15@gmail.com>
2023-11-30 12:59:51 -0800

    a program that calulates two numbers

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 import add_0
 
-# a program that calculates two values
-
 a = 1
 b = 2
-
-print("{} + {} = ".format(a, b), add_0.add(a, b))
+#<a value> + <b value> = <add(a, b) value>
+print("{a} + {b} = ".format(), add_0.add(a, b))
