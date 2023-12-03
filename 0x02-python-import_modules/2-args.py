fb3982a55a043a26bbe0e42c325d30ef9500f512
root <ifeanyitech15@gmail.com>
2023-12-02 17:33:58 -0800

    argv pogram

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys
+
+argument = 0
+length = len(sys.argv)
+
+if length == 0:
+    argument = 0
+    print("{} arguments.".format(argument))
+
+elif length == 1:
+    argument += 1
+
+    print(f"{argument} argument:")
+
+elif length > 1:
+    for i in range(1, len(sys.argv)):
+        argument += 1
+
+    print(f"{argument} arguments:")
+
+    for i in range(1, length):
+        print(f"{i}:", sys.argv[i])
