aae17958054a18ce6eb5176308410ef3bfb71bea
root <ifeanyitech15@gmail.com>
2023-12-04 08:48:09 -0800

    Secure access to an element in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,10 +2,18 @@
 
 def element_at(my_list, idx):
     current_list = []
+    lent_of_my_list = len(my_list) - 1
 
-    for i in range(len(my_list)):
-        current_list.append(my_list[i])
+    if idx < 0:
+        return None
 
-    for n in current_list:
-        if current_list[n] == current_list[idx]:
-            return current_list[n]
+    elif idx > lent_of_my_list:
+        return None
+
+    else:
+        for i in range(len(my_list)):
+            current_list.append(my_list[i])
+
+        for n in current_list:
+            if current_list[n] == current_list[idx]:
+                return current_list[n]
