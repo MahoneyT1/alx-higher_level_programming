ede5c4ce21720700561be80e1f2d50b263cda3c7
root <ifeanyitech15@gmail.com>
2023-12-04 14:41:26 -0800

    reverse list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    rl = []
-    lent = len(my_list)
-
-    for i in range(lent - 1, - 1, - 1):
-        rl.append(my_list[i])
-
-    for i in rl:
-        print("{:d}".format(i))
+    if my_list:
+        for i in my_list[::-1]:
+            print("{:d}".format(i))
