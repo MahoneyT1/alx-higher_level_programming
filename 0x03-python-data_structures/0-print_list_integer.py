b94d08286653b77dc047bc3b496d885ed12e9578
root <ifeanyitech15@gmail.com>
2023-12-04 05:15:55 -0800

    function to print contents of a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    newList = []
+
+    for i in range(0, len(my_list)):
+        newList.append(my_list[i])
+
+    for i in range(len(newList)):
+        print(newList[i])
