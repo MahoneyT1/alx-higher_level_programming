7d3c09abb354dd8d0bd8fab2e3c3a323ae658134
root <ifeanyitech15@gmail.com>
2023-12-05 01:46:12 -0800

    list of lists matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,12 @@
 def print_matrix_integer(matrix=[[]]):
     # Loop through the first list
     for row in matrix:
+
         # Loop through row in matrix:
+
         for elem in row:
-            print("{:d}".format(elem), end=', ')
+            print("{:d}".format(elem), end=' ')
+
         # Print space each time it loops
+
         print()
