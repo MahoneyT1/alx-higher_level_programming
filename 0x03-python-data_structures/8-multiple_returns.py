1dc6400169f52ae348487b7516d7478f9ca2b1a7
root <ifeanyitech15@gmail.com>
2023-12-05 17:40:58 -0800

    multiple return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -6,12 +6,13 @@ def multiple_returns(sentence):
     length_of_string = len(sentence)
 
     if length_of_string <= 0:
-        first == None
+        first_char = None
         return first
 
     else:
         # use a for loop to check the first char of the string
 
         for first in sentence:
-            if first == sentence[0]:
-                return (length_of_string, first)
+            if first > 0:
+                first_char = sentence[0]
+                return (length_of_string, first_char)
