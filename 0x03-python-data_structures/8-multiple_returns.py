55199897c83b3f7dbb71ec44dc4ffd96055cb240
root <ifeanyitech15@gmail.com>
2023-12-05 18:16:13 -0800

    multiple return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    """ A function that returns both lengeth of a string and the first char """
-
     if not sentence:
         sentence = None
 
