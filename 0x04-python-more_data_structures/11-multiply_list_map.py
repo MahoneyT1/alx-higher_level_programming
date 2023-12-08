00a5cf913b863c7818a13de61823548fd5a988ec
root <ifeanyitech15@gmail.com>
2023-12-08 05:51:33 -0800

    nulti ply the content of list with labda function

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new_list = list(map((lambda x: x * number), my_list.copy()))->str(new_list)
+    new_list = list(map((lambda x: x * number), my_list.copy()))
+    return new_list
