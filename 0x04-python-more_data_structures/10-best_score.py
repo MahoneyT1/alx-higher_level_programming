f092ebc43dbab5558662be4fa1ac334cb6f69878
root <ifeanyitech15@gmail.com>
2023-12-08 18:53:31 -0800

    using max to get the max number in a dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,9 +2,12 @@
 
 def best_score(a_dictionary):
     if a_dictionary:
-
-        for v in max(a_dictionary.items()):
-            return v
+        for k in a_dictionary.keys():
+            if k:
+                for k, v in max(a_dictionary.items()):
+                    return v
+            else:
+                return None
 
     else:
         return None
