db0a0b2f661e3dc1cf622a0cd9dd64ee277950ba
root <ifeanyitech15@gmail.com>
2023-12-08 16:12:14 -0800

    update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    key = str()
+    """ A function that replaces or adds key/value in a dictionary """
 
-    if key != str():
-        return True
+    if key in a_dictionary:
+        a_dictionary[key] = value
+        return a_dictionary
+
+    if key not in a_dictionary:
+        a_dictionary[key] = value
+        return a_dictionary
