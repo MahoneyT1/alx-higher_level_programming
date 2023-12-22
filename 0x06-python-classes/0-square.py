cf025a886ba8cce272c765a80d9c19df16111744
MahoneyT1 <ifeanyitech15@gmail.com>
2023-12-22 13:31:14 -0800

    update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
 class Square:
-    """ Write an empty class Square that defines a square """
+    """ Write an empty class Square that defines a square.
+args:
+    self:
+        one parameter
+    returns:
+            none
+    """
     def __init__(self) -> None:
         pass
