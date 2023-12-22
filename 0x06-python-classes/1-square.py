9d8afeba60ff3811417e0d3b335bf91213bb689b
MahoneyT1 <ifeanyitech15@gmail.com>
2023-12-22 14:09:32 -0800

    private attibute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" a function that defines square """
+
+class Square:
+    """ defining the private var in class """
+    def __init__(self, size):
+        self.__size = size
