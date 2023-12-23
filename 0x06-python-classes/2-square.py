7b627b23b4e212a9f32c7aa1381aac0eb05871e3
MahoneyT1 <ifeanyitech15@gmail.com>
2023-12-23 00:04:51 -0800

    using exception on oop

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" a class Square that defines a square """
+
+
+class Square:
+    def __init__(self, size=0):
+        """ privating he variable size """
+
+        self.__size = size
+
+        try:
+            if not isinstance(self.__size, int):
+                raise TypeError("size must be an integer")
+
+            if size < 0:
+                raise ValueError
+        except ValueError:
+            print("size must be >= 0")
