fe12ccc0bd4b9187c4d1537348451d7534d7be4f
MahoneyT1 <ifeanyitech15@gmail.com>
2023-12-24 05:16:08 -0800

    printing content of private attri

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" define a type class square """
+
+
+class Square:
+    """ representing the class square """
+
+    def __init__(self, size=0):
+        """ initializing a new obj with content of instance.
+
+
+        args:
+            size (int): size of square.
+            value : value to update the private attribute.
+
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """  value : value to update the private attribute """
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        for i in range(self.__size):
+            print("#" * self.__size)
+
+        else:
+            print()
