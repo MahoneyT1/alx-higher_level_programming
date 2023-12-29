43f3071dd8031b93634ceeb20adda739685ba7bf
MahoneyT1 <ifeanyitech15@gmail.com>
2023-12-28 19:50:51 -0800

    printing to standard err

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ import sys
 
 
 def safe_print_integer_err(value):
-    """ a function that prints an integer only.
+    """function that prints an integer only.
 
     if Value error occurs a correspoinding
     message is printed to standard error.
