2bc31fcf6161b531132cc2fea78fff53f93901d8
MahoneyT1 <ifeanyitech15@gmail.com>
2023-12-28 20:19:17 -0800

    printing to standard err

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -19,6 +19,6 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return (True)
     except (TypeError, ValueError):
-        print("Exception: {}".format(sys.exc_info()[1], \
-                sys.stderr))
+        print("Exception:", sys.exc_info()[1], \
+                file=sys.stderr)
         return(False)
