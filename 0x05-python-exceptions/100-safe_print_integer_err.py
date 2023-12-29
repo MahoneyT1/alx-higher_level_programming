1a57405448af495dd78d1f6f2ae67a1bc08c438d
MahoneyT1 <ifeanyitech15@gmail.com>
2023-12-28 17:10:04 -0800

    stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import sys
+
 
 
 def safe_print_integer_err(value):
@@ -8,6 +8,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError as e:
+
+    except Exception as e:
         print("Exception: ", e)
         return False
