b805ef7f2693ae3ab0aec4670e3007896e90f3d0
MahoneyT1 <ifeanyitech15@gmail.com>
2024-01-01 06:03:24 -0800

    fizzbuss

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
-    """ function that prints the numbers from 1 to \
-        100 separated by a space """
-
     for i in range(1, 101):
 
         if i % 3 == 0 and i % 5 == 0:
