0b55155dc0fbb5cb32b5e016e701f164758f111e
MahoneyT1 <ifeanyitech15@gmail.com>
2024-01-08 22:49:16 -0800

    if it is type of the same class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
 def is_same_class(obj, a_class):
-    """ A function that return True if obj is a an obj of the\
-            same cls.
+    """ A function that return True if obj is a an obj of the,
+        same cls.
 
-        Args:
-            obj(obj): param 1 object to check
-            a_class(cls): param 2 class to check.
+    Args:
+        obj(obj): param 1 object to check
+        a_class(cls): param 2 class to check.
 
-        Returns:
-            True if obj is object of the same class
-            False if it is not.
+    Returns:
+        True if obj(any)is object of the same class
+        False if it is not.
 
     """
     if type(obj) == a_class:
