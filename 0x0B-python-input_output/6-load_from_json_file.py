a1901a8ff1b07890d9c348d98ded8cf0161e98be
MahoneyT1 <ifeanyitech15@gmail.com>
2024-01-10 21:40:20 -0800

    update

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" funtion that reads js file """
+import json
+
+
+def load_from_json_file(filename):
+    """function that creates an Object from a JSON file.
+
+    Args:
+        filename(path): param1
+    Return:
+        read
+    """
+    with open(filename) as f:
+        g = json.load( f )
+        return g
