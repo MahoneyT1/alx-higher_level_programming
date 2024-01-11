85943e47aabd73dfd10565719a2df007a39fef24
MahoneyT1 <ifeanyitech15@gmail.com>
2024-01-11 05:04:47 -0800

    update

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-""" Write a function that returns the dictionary description,
-with simple data structure.
-"""
+""" Write a function that returns destription"""
+
+
 def class_to_json(obj):
+    """ return obj description.
+
+    Args:
+        obj(any): param 1
+    """
+
     return obj.__dict__
