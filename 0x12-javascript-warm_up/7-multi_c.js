5f3b3cee05e04899577556afe3c0cd99e4ae06bc
MahoneyT1 <ifeanyitech15@gmail.com>
2024-03-12 11:50:59 -0700

    update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,17 +2,14 @@
 // Write a script that prints x times “C is fun”.
 
 const x = process.argv[2];
+const parsedIn = parseInt(x, 10);
 
-if (!isNaN(x)) {
-  const result = parseInt(x);
-
-  if (Number.isInteger(result) && result >= 0) {
-    for (let i = 0; i < result; i++) {
-      console.log('C is fun');
-    }
-  } else {
-    console.log('Missing number of occurrences');
-  }
-} else {
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < parsedIn) {
+    console.log('C is fun');
+    i++;
+  }
 }
