61aeaa100acc28361155899587d21bbec696f717
MahoneyT1 <ifeanyitech15@gmail.com>
2024-03-13 07:51:13 -0700

    update

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 
+function add (a, b) {
+  if (Number.isInteger(a) || Number.isInteger(b)) {
+    return a + b;
+  }
+}
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
-if ((!isNaN(arg1) || !isNaN(arg2)) &&
-  (Number.isInteger(arg1) || Number.isInteger(arg2))) {
-  const first = parseInt(arg1, 10);
-  const second = parseInt(arg2, 10);
-  const sum = add(first, second);
-  console.log(sum);
-} else {
+if (isNaN(arg1) || isNaN(arg2)) {
   console.log('NaN');
-}
-function add (a, b) {
-  return a + b;
+} else {
+  const first = parseInt(arg1);
+  const second = parseInt(arg2);
+  const result = add(first, second);
+  console.log(result);
 }
