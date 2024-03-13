b3cd5147c5541ddc9ac93a8b5a26a40623d00d49
MahoneyT1 <ifeanyitech15@gmail.com>
2024-03-13 02:38:37 -0700

    add through function

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,13 +5,14 @@ const com = process.argv;
 if (com.length < 4) {
   console.log(NaN);
 }
-const first = parseInt(com[2]);
-const second = parseInt(com[3]);
+const first = parseInt(com[2], 10);
+const second = parseInt(com[3], 10);
 
 function add (a, b) {
   if (isNaN(a) === true || isNaN(b) === true) {
     return NaN;
-  } return a + b;
+  } 
+   return a + b;
 }
 const sum = add(first, second);
 console.log(sum);
