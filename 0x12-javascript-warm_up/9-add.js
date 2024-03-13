9ccd59d71e5ae60c1e4beecbad82a030586b5ee5
MahoneyT1 <ifeanyitech15@gmail.com>
2024-03-13 07:24:58 -0700

    update

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,20 +1,17 @@
 #!/usr/bin/node
-//Write a script that prints the addition of 2 integers
 
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
-if (!isNaN(arg1) || !isNaN(arg2)) {
-    const first = parseInt(arg1, 10);
-    const second = parseInt(arg2, 10);
-
-    let sum = add(first, second);
-    console.log(sum);
-
+if ((!isNaN(arg1) || !isNaN(arg2)) &&
+  (Number.isInteger(arg1) || Number.isInteger(arg2))) {
+  const first = parseInt(arg1, 10);
+  const second = parseInt(arg2, 10);
+  const sum = add(first, second);
+  console.log(sum);
 } else {
-    console.log(NaN)
+  console.log(NaN);
 }
-
-function add(a, b) {
-    return a + b;
+function add (a, b) {
+  return a + b;
 }
