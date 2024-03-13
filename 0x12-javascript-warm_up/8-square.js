adb0360a4a0c7c1b03bb0418856d1b740daa8e5b
MahoneyT1 <ifeanyitech15@gmail.com>
2024-03-13 08:21:42 -0700

    update

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,19 +1,16 @@
 #!/usr/bin/node
 
-const first = process.argv[2];
-const result = parseInt(first, 10);
+const arg = process.argv;
 
 // check if input is Not a Number or else
-if (isNaN(result)) {
+if (isNaN(arg[2])) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < result; i++) {
-    // create a string to contain your concat strings
-    let ask = '';
-
-    for (let j = 0; j < result; j++) {
-      ask += 'x' + ' ';
+  for (let i = 0; i <= arg.length; i++) {
+    let strCon = '';
+    for (let j = 0; j <= arg.length; j++) {
+      strCon += 'x' + ' ';
     }
-    console.log(ask);
+    console.log(strCon);
   }
 }
