d5663a23c07e0715644f82cf52df97b8a69683a2
MahoneyT1 <ifeanyitech15@gmail.com>
2024-03-13 09:46:02 -0700

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (isNaN(n) || n < 0 || !Number.isInteger(n)) {
+    return 1;
+  } else if (isNaN(n) || n <= 1 || !Number.isInteger(n)) {
+    return 1;
+  } else if (!isNaN(n) || n > 1 || Number.isInteger(n)) {
+    return n * factorial(n - 1);
+  }
+}
+
+const first = process.argv[2];
+const temp = parseInt(first);
+
+if (!isNaN(temp) && Number.isInteger(temp)) {
+  console.log(factorial(temp));
+}
