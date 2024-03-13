3b5bb2ad00b25a7aae385c9a21a24fae292ac188
MahoneyT1 <ifeanyitech15@gmail.com>
2024-03-13 01:46:19 -0700

    add through function

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const com = process.argv;
+
+if (com.length < 4 ) {
+    console.log(NaN);
+    return
+}
+const first = parseInt(com[2]);
+const second = parseInt(com[3]);
+
+function add(a, b) {
+    return a + b;
+}
+
+let sum = add(first, second);
+console.log(sum);
