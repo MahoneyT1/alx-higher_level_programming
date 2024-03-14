7d917d7c10ba7578db3fed2db472b7f31dcbca84
MahoneyT1 <ifeanyitech15@gmail.com>
2024-03-14 06:52:28 -0700

    update

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,12 +4,15 @@
 
 const myObject = {
   type: 'object',
-  value: 12,
-  incr: function () {
-    this.value++;
-  }
+  value: 12
 };
 console.log(myObject);
+
+function incr () {
+  this.value++;
+}
+myObject.incr = incr;
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
