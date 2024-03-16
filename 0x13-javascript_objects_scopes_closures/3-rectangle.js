7b29162ff112de6146eb5a4786683d7a3b70291f
MahoneyT1 <ifeanyitech15@gmail.com>
2024-03-16 10:36:41 -0700

    update

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,26 +1,23 @@
 #!/usr/bin/node
 
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-    } else {
-      return {};
     }
   }
 
   print () {
-    if (this.width && this.height) {
-      let newStr;
-      for (let i = 0; i < this.height; i++) {
-        newStr = '';
-        for (let j = 0; j < this.width; j++) {
-          newStr += 'x';
-        }
-        console.log(newStr);
+    let rectChar; // declare var to act as container for printing charcaters
+    for (let i = 0; i < this.height; i++) {
+      // let the variable be an empty string
+      rectChar = '';
+      for (let j = 0; j < this.width; j++) {
+        // append the X char to the variable in each iteration
+        rectChar += 'X';
       }
+      console.log(rectChar);
     }
   }
-}
-module.exports = Rectangle;
+};
