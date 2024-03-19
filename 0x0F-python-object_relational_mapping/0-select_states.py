9f4dfb5013edaed7e6dc01b9c9c795efd3448446
MahoneyT1 <ifeanyitech15@gmail.com>
2024-03-19 13:09:31 +0000

    rite a script that lists all states from the database hbtn_0e_0_usa:

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+""" Write a script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 from sys import argv
-""" Write a script that lists all states from the database hbtn_0e_0_usa"""
+
 
 
 if __name__ == '__main__':
