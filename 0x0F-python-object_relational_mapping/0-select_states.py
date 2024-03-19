c29994ca1856ff6c20a385aee5c1f209902e59d9
MahoneyT1 <ifeanyitech15@gmail.com>
2024-03-19 15:07:52 +0000

    a script that lists all states with a name starting with N (upper N) from the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
+""" Write a script that lists all states from the database hbtn_0e_0_usa """
 
-""" Write a script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    # establishing a secure connection to the MySQL server
+    
+    #connect the database
     db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-    )
+            host='localhost',
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    #create a cursor obect
+    mycursor = db.cursor()
 
-    # creating a cursor object to execute SQL queries
-    cursor = db.cursor()
+    #execute command using cursor a reference to database
+    mycursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # executing the cursor to retrieve states sorted by id
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = mycursor.fetchall()
 
-    # fetching all the results
-    states = cursor.fetchall()
+    for row in rows:
+        print(row)
 
-    # display/print them out
-    for state in states:
-        print(state)
-
-    # closing the cursor and database connection
-    cursor.close()
+    #close both cursor and database
+    mycursor.close()
     db.close()
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Write a script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    # connect to the database
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    # create a cursor object as a reference to the database
+    mycursor = db.cursor()
+
+    # fetch data from data-base by name starting with N
+
+    mycursor.execute("SELECT id, name FROM states WHERE name LIKE 'N%'
+                     ORDER BY id ASC")
+    # print the roles
+
+    rows = mycursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    # close database and cursor
+
+    mycursor.close()
+    db.close()
