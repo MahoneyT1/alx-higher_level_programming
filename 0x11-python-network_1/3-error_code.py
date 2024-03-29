2edcc3638e37f686d1a27a017eb98283227705a9
MahoneyT1 <ifeanyitech15@gmail.com>
2024-03-29 10:16:01 -0700

    urlerror

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,12 +4,11 @@ a Python script that takes in a URL, sends
 """
 import urllib.request
 from sys import argv
-import  urllib.error
+import urllib.error
 
 
 if __name__ == "__main__":
     url = argv[1]
-
 
     try:
         with urllib.request.urlopen(req) as response:
