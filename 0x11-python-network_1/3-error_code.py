c4d757699d69b4e2840bf07c9709fc046e4003ce
MahoneyT1 <ifeanyitech15@gmail.com>
2024-03-29 09:55:16 -0700

    urlerror

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     try:
         with urllib.request.urlopen(req) as response:
             print(response.read().decode('utf-8'))
-    except URLError as e:
+    except urllib.error.HTTPError as e:
         print("Error code:", e.code)
