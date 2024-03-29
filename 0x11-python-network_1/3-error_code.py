afa051728a1b02cc7c4e2be4a487cb683ef90342
MahoneyT1 <ifeanyitech15@gmail.com>
2024-03-29 10:18:34 -0700

    urlerror

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""
-a Python script that takes in a URL, sends
+
+"""Script that takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8).
+Usage: ./3-error_code.py <URL>
+  - Handles HTTP errors.
 """
 import urllib.request
 from sys import argv
