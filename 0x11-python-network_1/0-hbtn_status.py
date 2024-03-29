4c1307a5750d720733d94a79ebe2066455041886
Your Name <ifeanyitech15@gmail.com>
2024-03-29 02:45:09 +0000

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
+""" A python script that fetches https:alx-intranet.hbtn.io/status """
 
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-    print("Body response:")
-    print("\t- type:", type(html))
-    print("\t- content:", html)
-    print("\t- utf8 content:", html.decode('utf-8'))
+if __name__ == '__main__':
+    custom_request = 'https://alx-intranet.hbtn.io/status'
+
+    with urllib.request.urlopen(custom_request) as response:
+        data = response.read()
+        print('Body response:')
+        print("\t - type: {}", type(data))
+        print("\t - content: {}", data)
+        print("\t - utf8 content: {}", data.decode('utf-8'))
