493726af1e67d55fffc62014e1a6997e839a3edc
Your Name <ifeanyitech15@gmail.com>
2024-03-29 03:04:18 +0000

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ custom_request = 'https://alx-intranet.hbtn.io/status'
 with urllib.request.urlopen(custom_request) as response:
     html = response.read()
     print("Body response:")
-    print("\t - type:", type(html))
-    print("\t - content:", data)
-    print("\t - utf8 content:", htlm.decode('utf-8'))
+    print("\t- type:", type(html))
+    print("\t- content:", html)
+    print("\t- utf8 content:", html.decode('utf-8'))
