62dced165fef901e06d771878aa06d267edaa129
MahoneyT1 <ifeanyitech15@gmail.com>
2024-03-29 10:22:47 -0700

    urlerror

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,16 +5,16 @@ and displays the body of the response (decoded in utf-8).
 Usage: ./3-error_code.py <URL>
   - Handles HTTP errors.
 """
-import urllib.request
-from sys import argv
+import sys
 import urllib.error
+import urllib.request
 
 
 if __name__ == "__main__":
-    url = argv[1]
+    url = sys.argv[1]
 
     try:
-        with urllib.request.urlopen(req) as response:
+        with urllib.request.urlopen(url) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
