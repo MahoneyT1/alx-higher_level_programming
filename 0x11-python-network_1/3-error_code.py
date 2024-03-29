96c1ff04545a01af48ec0b1adafedebbcfca5fa5
MahoneyT1 <ifeanyitech15@gmail.com>
2024-03-29 09:49:49 -0700

    urlerror

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL, sends a request
+to the URL and displays the body of the response
+(decoded in utf-8)
+"""
+
+import urllib.request
+from sys import argv
+from urllib.error import URLError, HTTPError
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except URLError as e:
+        print("Error code:", e.code)
