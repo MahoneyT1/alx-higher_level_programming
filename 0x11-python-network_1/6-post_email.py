3fbab97bc391f5014781334a7475682ea59a1a2d
Your Name <ifeanyitech15@gmail.com>
2024-03-30 14:18:22 +0000

    post requests

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,7 +10,7 @@ from sys import argv
 
 if __name__ == '__main__':
     url = argv[1]
-    email = {'email':argv[2]}
+    email = {'email': argv[2]}
 
     req = requests.post(url, data=email)
     print(req.text)
