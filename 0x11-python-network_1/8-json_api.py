43ea701f389c37e94c48ed5818990e01b532df4b
Your Name <ifeanyitech15@gmail.com>
2024-03-30 16:46:09 +0000

    post request

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+""" Write a Python script that takes in a letter and sends a POST
+request to http://0.0.0.0:5000/search_user with the letter as a
+parameter.The letter must be sent in the variable q
+If no argument is given, set q=""
+If the response body is properly JSON formatted and not empty,
+display the id and name like this: [<id>] <name>
+Otherwise:
+Display Not a valid JSON if the JSON is invalid
+Display No result if the JSON is empty
+You must use the package requests and sys
+You are not allowed to import packages other than requests and sys
+"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    if not argv[1]:
+        q = ""
+
+    q = argv[1]
+    url = 'http://0.0.0.0:5000/search_user'
+    q = {'q': q}
+
+    try:
+        resp = requests.post(url, data=q)
+
+        resp = resp.json()
+
+        if resp:
+            print("[{}] {}".format(resp['id'], resp['name']))
+        else:
+            print("Not a valid JSON")
+    except ValueError:
+        print("No result")
