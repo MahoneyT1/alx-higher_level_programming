3495c355d7a710d456446b9c650c15bd4e3c6bfa
MahoneyT1 <ifeanyitech15@gmail.com>
2024-05-01 02:21:57 -0700

    count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,44 +1,20 @@
 #!/usr/bin/node
-// Write a script that prints the number of movies where
-// the character “Wedge Antilles” is present.
 
-// The first argument is the API URL of the Star wars API: https://swapi-api.alx-tools.com/api/films/
-// Wedge Antilles is character ID 18 - your script must use this ID for filtering the result of the API
-// You must use the module request
+const request = require('request');
 
-function numberOfMovies (url) {
-  let count = 0;
-  // import request
-  const request = require('request');
+const apiUrl = process.argv[2];
+const characterId = 18;
 
-  // send a request to the received api
-  request(url, (error, response, body) => {
-    // check if there's error while making the request
-    if (error) {
-      // print to standard error the error output
-      console.error('Error:', error);
-    } else {
-      const receivedData = JSON.parse(body);
-
-      // loop through the receivedData and capture characters
-      for (let i = 0; i < receivedData.results.length; i++) {
-        if (Object.prototype.hasOwnProperty.call(receivedData.results[i], 'characters')) {
-          const listOfCharacters = receivedData.results[i].characters;
-
-          // create an end point, and check if the id 18 is present in the list
-          const urlToId = 'https://swapi-api.alx-tools.com/api/people/18/';
-
-          // loop and increment count if the url matches any url in the list
-          for (let j = 1; j <= listOfCharacters.length; j++) {
-            if (listOfCharacters[j] === urlToId) {
-              count++;
-            }
-          }
-        }
-      }
-      console.log(count);
-    }
-  });
-}
-const argvApi = process.argv[2];
-numberOfMovies(argvApi);
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const filmsData = JSON.parse(body).results;
+    const moviesWithWedge = filmsData.filter((film) =>
+      film.characters.includes(
+        `https://swapi-api.alx-tools.com/api/people/${characterId}/`
+      )
+    );
+    console.log(moviesWithWedge.length);
+  }
+});
