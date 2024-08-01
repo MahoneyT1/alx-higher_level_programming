7cb811a979d81d882baae5066deb847c0351cdb7
MahoneyT1 <ifeanyitech15@gmail.com>
2024-08-01 08:05:55 +0100

    write to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+/*
+* Write a script that writes a string to a file.
+* The first argument is the file path
+* The second argument is the string to write
+* The content of the file must be written in utf-8
+* If an error occurred during while writing, print the error object
+*/
+
+const fs = require('fs').promises;
+
+const stringArg = process.argv[3];
+const filePath = process.argv[2];
+
+async function writeToFile (filePath, stringArg) {
+  try {
+    await fs.writeFile(filePath, stringArg, 'utf-8');
+    console.log('sucessfully written to file');
+  } catch (error) {
+    console.error(error);
+  }
+}
+writeToFile(filePath, stringArg);
