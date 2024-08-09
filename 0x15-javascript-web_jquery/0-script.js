435ef8bb1b8a2886c7e0f3070670fc6ef4682c5f
MahoneyT1 <ifeanyitech15@gmail.com>
2024-08-09 20:39:29 +0100

    J query

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+// Write a JavaScript script that updates the text color of the
+// <header> element to red (#FF0000)
+
+
+
+const header = document.querySelector('header').style.color = 'red';
+
